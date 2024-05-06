296be57a5f58d9c27ffd1d6f7d3f888b36dfb4ca
vladimirmac <vladimirbodrin89@gmail.com>
2024-05-06 17:36:57 +0400

    FIX click option --host

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from src.create_app import create_app
 
 
 @click.command()
-@click.option('--host', default='0.0.0.0', help='Host IP')
+@click.option('--host', default='localhost', help='Host IP')
 @click.option('--port', default=8000, help='Port number')
 def run_server(host, port):
     app = create_app()
